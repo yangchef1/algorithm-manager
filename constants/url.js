6d268dc106f38330162920a8442571cd9ae715dc
yang <hc1489@naver.com>
2025-01-31 23:31:45 +0900

    refactor: 상수 종류 별 파일 분리

---
+++ constants/url.js
@@ -0,0 +1,10 @@
+export const PROXY_URL =
+  "https://panicky-arabella-cors-anywhere-c4eef9e3.koyeb.app/proxy";
+export const BOJ_PROBLEM_URL = "https://www.acmicpc.net/problem/";
+
+export const TIER_IMAGE_BASE_URL =
+  "https://d2gd6pc034wcta.cloudfront.net/tier/";
+
+export const SOLVED_AC_URL = "https://solved.ac/api/v3/search/problem";
+export const NOTION_PAGE_URL = "https://api.notion.com/v1/pages/";
+export const NOTION_DARABASE_URL = "https://api.notion.com/v1/databases/";
