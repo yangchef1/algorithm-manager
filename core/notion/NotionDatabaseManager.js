8f144a3e52ac0906cd9e7b8d152f45462359806c
yang <hc1489@naver.com>
2025-01-31 23:33:25 +0900

    feat: Notion DB 스키마 변경 및 문제 추가 기능 구현

---
+++ core/notion/NotionDatabaseManager.js
@@ -0,0 +1,75 @@
+import {
+  NOTION_DARABASE_URL,
+  NOTION_PAGE_URL,
+  PROXY_URL,
+} from "../../constants/url.js";
+import { sleep } from "../../utils/sleep.js";
+
+export class NotionDatabaseManager {
+  constructor() {}
+
+  async addProblem(problem) {
+    await fetch(PROXY_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        url: NOTION_PAGE_URL,
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem("notionApiToken")}`,
+          "Notion-Version": "2022-06-28",
+        },
+        body: {
+          parent: { database_id: sessionStorage.getItem("notionDatabaseId") },
+          properties: problem.toJson(),
+        },
+      }),
+    });
+
+    await sleep(500);
+  }
+
+  async setSchema() {
+    await fetch(PROXY_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        url: `${NOTION_DARABASE_URL}${sessionStorage.getItem(
+          "notionDatabaseId"
+        )}`,
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem("notionApiToken")}`,
+          "Notion-Version": "2022-06-28",
+        },
+        body: {
+          parent: { database_id: sessionStorage.getItem("notionDatabaseId") },
+          properties: {
+            알고리즘: {
+              multi_select: {},
+            },
+            문제: {
+              url: {},
+            },
+            난이도: {
+              select: {},
+            },
+            "풀이 시간 (분)": {
+              number: {},
+            },
+            "푼 날짜": {
+              date: {},
+            },
+            비고: {
+              text: {},
+            },
+          },
+        },
+      }),
+    });
+  }
+}
